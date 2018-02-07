132077ec8801c9de06cb8f291b4b5c9e50bf2002
Cameron Abrams <cameronabrams@gmail.com>
2018-02-07 15:13:58 -0500

    fixed sucrose extraction

--- sucr/extractpdb.tcl
+++ sucr/extractpdb.tcl
@@ -1,16 +1,15 @@
 mol new my_sucr_i.psf
 mol addfile [lindex $argv 0] 
 
-set a [atomselect top "resname ALGC BFRU"]
-
-$a set segname SU
-$a set chain S
 
 set aglc [atomselect top "resname AGLC"]
 $aglc set resid 1
 set bfru [atomselect top "resname BFRU"]
 $bfru set resid 2
 
+set a [atomselect top "resname AGLC BFRU"]
+$a set segname SU
+$a set chain S
 $a writepdb "my_sucr_su.pdb"
 
 exit
