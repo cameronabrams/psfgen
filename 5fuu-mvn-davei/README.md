57f780eaa4d1818d8669254bbde9f3251c6b68db
Cameron Abrams <cameronabrams@gmail.com>
2018-09-01 15:44:39 -0400

    5fuu-davei

---
+++ 5fuu-mvn-davei/README.md
@@ -0,0 +1,13 @@
+# MVN DAVEI docked to HIV-1 Env
+
+## Instructions
+
+1. generate a 5fuu model with MPER's and Man9's
+2. Note which two AMAN residues you want to try to dock the MVN onto
+3. generate a 2yhh MVN(mut)-DAVEI
+4. go to a clean directory;
+```
+$ mkdir my_complex
+$ cd my_complex
+$ $PSFGEN_BASEDIR/5fuu-mvn-davei/do.sh
+```
--- 5fuu-mvn-davei/do.sh
+++ 5fuu-mvn-davei/do.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-echo $CHARMRUN
-echo $NAMD2
-vmd -dispdev text -e mkpsf.tcl > psfgen1.log
+# check for base directory name variable;
+# if not set, use default
+if {![info exists PSFGEN_BASEDIR]} {
+  # see if user set an environment variable
+  if {[info exists env(PSFGEN_BASEDIR)]} {
+      set PSFGEN_BASEDIR $env(PSFGEN_BASEDIR)
+  } else {
+      set PSFGEN_BASEDIR $env(HOME)/research/psfgen
+  }
+}
+
+SYSNAME=5fuu-mvn-davei
+vmd -dispdev text -e $PSFGEN_BASEDIR/${SYSNAME}/mkpsf.tcl > psfgen1.log
+cp $PSFGEN_BASEDIR/${SYSNAME}/my_complex_vac_stage0.namd .
 $CHARMRUN +p2 $NAMD2 my_complex_vac_stage0.namd > vac0.log
+cp $PSFGEN_BASEDIR/${SYSNAME}/my_complex_vac_stage1.namd .
 $CHARMRUN +p2 $NAMD2 my_complex_vac_stage1.namd > vac1.log
 echo "Done."
 
