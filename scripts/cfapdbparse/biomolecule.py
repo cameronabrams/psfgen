2c181a18363f6e1871b01a4a2d7b163b804cffdf
Cameron Abrams <cfa22@drexel.edu>
2020-11-01 18:47:49 -0500

    bio

--- scripts/cfapdbparse/biomolecule.py
+++ scripts/cfapdbparse/biomolecule.py
@@ -18,6 +18,7 @@ class Biomolecule:
        self.fe_units='None'
        self.chains=[]
        if 'IDENTITY' in pdbrecord:
+           print('#### identity biomt requested')
            self.index=1
            self.biomt.append(BiomT())
     def parsePDBrecordwords(self,words):
