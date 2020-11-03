597ab8aca0192266fd7a7b645d0d68688a3a881f
Cameron Abrams <cfa22@drexel.edu>
2020-11-03 10:46:49 -0500

    py

--- scripts/cp_charmm.sh
+++ scripts/cp_charmm.sh
@@ -9,9 +9,17 @@ if [ -f par.inp ]; then
    rm par.inp
 fi
 touch par.inp
-for f in `grep ^parameters $CONF | awk -F\) '{print $2}'`; do
-  cp ${HOME}${f} .
-  ff=`echo $f | awk -F'/' '{print $NF}'`
+for f in `grep ^parameters $CONF | awk '{print $2}'`; do
+  ev=`echo $f | awk -F'/' '{print $1}'`
+  if [[ $ev == '$env(HOME)' ]]; then
+      pre=${HOME}
+  fi
+  if [[ $ev == '$env(PSFGEN_BASEDIR)' ]]; then
+      pre=${PSFGEN_BASEDIR}
+  fi  
+  suf=`echo $f | cut -d/ -f 2-`
+  cp $pre/$suf .
+  ff=`echo $suf | awk -F'/' '{print $NF}'`
   echo "parameters $ff" >> par.inp
 done
 
