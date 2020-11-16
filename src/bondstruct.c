156b6363c249571574fd6e60a4b95be4fde11475
Abrams <cfa22@drexel.edu>
2020-11-16 09:16:42 -0500

    cifwork

--- src/bondstruct.c
+++ src/bondstruct.c
@@ -208,6 +208,7 @@ void bondstruct_makerotatablebondlist ( bondstruct * bs, int * rot_i, int ni, in
        }
      }
    }  
+   printf('#### calling bondstruct_makerightsides');fflush(stdout);
    bondstruct_makerightsides(bs);
 }
 
