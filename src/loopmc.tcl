3aeeb93742a7011e882c4c4dbffadd835cf3cea5
Abrams <cfa22@drexel.edu>
2020-11-16 16:34:53 -0500

    cifwork

--- src/loopmc.tcl
+++ src/loopmc.tcl
@@ -210,10 +210,14 @@ proc irand_dom { min max } {
 # logs frames to a logging molecule
 proc log_addframe { molid logid } {
    if { $logid != "-1" } {
-     [atomselect $molid all] writepdb "tmp.pdb"
-     animate read pdb tmp.pdb $logid
+     animate dup $logid
+     [atomselect $logid all] set x [[atomselect $molid all] get x]
+     [atomselect $logid all] set y [[atomselect $molid all] get y]
+     [atomselect $logid all] set z [[atomselect $molid all] get z]
+#     [atomselect $molid all] writepdb "tmp.pdb"
+#     animate read pdb tmp.pdb $logid
      puts "Molid $molid - logging molecule $logid has [molinfo $logid get numframes] frames."
-     exec rm -f tmp.pdb
+#     exec rm -f tmp.pdb
    }
 }
 
