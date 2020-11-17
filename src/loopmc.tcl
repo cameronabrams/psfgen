394f60b16c329611a3456a52d4ac0249f565befa
Abrams <cfa22@drexel.edu>
2020-11-17 14:54:58 -0500

    rotwork

--- src/loopmc.tcl
+++ src/loopmc.tcl
@@ -62,7 +62,7 @@ proc roughenergy { sel1 sel2 cut sigma epsilon bs }  {
    set E [my_roughenergy $r1 $x1 $y1 $z1 [$sel1 num] $r2 $x2 $y2 $z2 [$sel2 num] $cut $sigma $epsilon $bs]
    delete_arrayint $r1
    delete_arrayint $r2
-   delete_array $x1
+   #delete_array $x1
    delete_array $x2
    delete_array $y1
    delete_array $y2
