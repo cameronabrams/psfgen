66cf6201a09e7686853802b0d276f3765a377178
Abrams <cfa22@drexel.edu>
2020-11-16 20:17:23 -0500

    cifwork

--- src/bondstruct.c
+++ src/bondstruct.c
@@ -252,23 +252,22 @@ double my_roughenergy ( int * i1, double * x1, double * y1, double * z1, int n1,
    int i,j;
    double cut2=cut*cut;
    double d2,E=0.0;
-   s6=sigma*sigma*sigma*sigma*sigma*sigma;
-   rcut=pow(2,1./6.)*sigma
+   double s6=sigma*sigma*sigma*sigma*sigma*sigma;
+   double rcut=pow(2,1./6.)*sigma;
    if (rcut>cut) {
-      rcut=cut
+      rcut=cut;
    }
-   rcut2=rcut*rcut
+   rcut2=rcut*rcut;
    for (i=0;i<n1;i++) {
       for (j=0;j<n2;j++) {
          if (i1[i]!=i2[j]) {
            d2 =(x1[i]-x2[j])*(x1[i]-x2[j]);
            d2+=(y1[i]-y2[j])*(y1[i]-y2[j]);
            d2+=(z1[i]-z2[j])*(z1[i]-z2[j]);
-           
            if (d2<rcut2) {
-              di6=s6/(d2*d2*d2)
-              di12=di6**2
-              E+=4*epsilon*(di12-di6)+epsilon
+              di6=s6/(d2*d2*d2);
+              di12=di6**2;
+              E+=4*epsilon*(di12-di6)+epsilon;
            }
          }
       }
