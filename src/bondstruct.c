2a79240968c5c6c6852e23af11c53b335bfdc852
Abrams <cfa22@drexel.edu>
2020-11-17 21:11:48 -0500

    rotwork

--- src/bondstruct.c
+++ src/bondstruct.c
@@ -322,7 +322,7 @@ double my_roughenergy ( int * i1, double * x1, double * y1, double * z1, int n1,
    for (i=0;i<n1;i++) {
       // pairwise sum over all sel1 pairs
       a=i1[i];
-      printf(" %i intrasel\n",a);fflush(stdout);
+      //printf(" %i intrasel\n",a);fflush(stdout);
       for (ij=i+1;ij<n1;ij++) {
          b=i1[ij];
          if (bondstruct_arebonded(bs,a,b)) continue;
@@ -339,17 +339,17 @@ double my_roughenergy ( int * i1, double * x1, double * y1, double * z1, int n1,
       icx=(int)((x1[i]-ls->xmin)/ls->dx);
       icy=(int)((y1[i]-ls->ymin)/ls->dy);
       icz=(int)((z1[i]-ls->zmin)/ls->dz);
-      printf(" %i cell(%i,%i,%i)\n",a,icx,icy,icz);fflush(stdout);
+      //printf(" %i cell(%i/%i,%i/%i,%i/%i)\n",a,icx,ls->xnc,icy,ls->ync,icz,ls->znc);fflush(stdout);
       for (dx=-1;dx<2;dx++) {
          tx=icx+dx;
          if (tx==ls->xnc||tx==-1) continue;
          for (dy=-1;dy<2;dy++) {
-            ty=icy+dx;
+            ty=icy+dy;
             if (ty==ls->ync||ty==-1) continue;
             for (dz=-1;dz<2;dz++) {
                tz=icy+dz;
                if (tz==ls->znc||tz==-1) continue;
-               printf("   -> cell(%i,%i,%i)\n",tx,ty,tz);fflush(stdout);
+               //printf("   -> cell(%i/%i,%i/%i,%i/%i)\n",tx,ls->xnc,ty,ls->ync,tz,ls->znc);fflush(stdout);
                pa=ls->pa[tx][ty][tz];
                np=ls->np[tx][ty][tz];
                for (j=0;j<np;j++) {
@@ -371,6 +371,7 @@ double my_roughenergy ( int * i1, double * x1, double * y1, double * z1, int n1,
          }
       }
    }
+   print("Returning %.5f\n",E*epsilon);
    return E*epsilon;
 }
 
