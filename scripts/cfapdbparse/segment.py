cd19d8b5e052a7f272cf9c629d40022630144325
Abrams <cfa22@drexel.edu>
2020-11-19 18:53:07 -0500

    rotwork

--- scripts/cfapdbparse/segment.py
+++ scripts/cfapdbparse/segment.py
@@ -222,6 +222,7 @@ class Segment:
                     else:
                         if (ss.sacrins!='0' and i>0 and i<(len(self.subsegbounds)-1)):
                             fragss=self.subsegbounds[i+1]
+                            ss.nextres=fragss.d.resseqnum1
                             stanzastr+='patch cneu {}:{}{}\n'.format(rep_segname,l.residues[-1].resseqnum,l.residues[-1].insertion)
                             stanzastr+='patch nneu {}:{}\n'.format(rep_segname,fragss.d.resseqnum1)
                             stanzastr+='delatom {} {}{}\n'.format(rep_segname,l.residues[-1].resseqnum,ss.sacrins)
