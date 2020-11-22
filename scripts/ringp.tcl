33c64cc7bbafe3c7171118d5692b20391be949d6
Abrams <cfa22@drexel.edu>
2020-11-22 16:50:26 -0500

    r

--- scripts/ringp.tcl
+++ scripts/ringp.tcl
@@ -1,6 +1,8 @@
 source /home/cfa/research/psfgen/src/loopmc.tcl
 set psf [lindex $argv 0]
 set namdbin [lindex $argv 1]
+mol new $psf
+mol addfile $namdbin waitfor all
 check_pierced_rings 0 6 1.5
 check_pierced_rings 0 5 1.5
 exit
