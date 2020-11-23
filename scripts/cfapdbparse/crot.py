0e43ac6d32a84e1c4c422ebc68bec3c06cdc674a
Abrams <cfa22@drexel.edu>
2020-11-22 20:16:15 -0500

    r

--- scripts/cfapdbparse/crot.py
+++ scripts/cfapdbparse/crot.py
@@ -1,7 +1,5 @@
 class Crot:
-    ''' user-defined rotation of phi/psi/chiX bonds of
-        specific residue to reorient all atoms C-terminal
-        to that residue up to a designated end residue 
+    ''' user-defined rotation and translations
     '''
     def __init__(self,record):
         self.record=record
@@ -29,6 +27,24 @@ class Crot:
             self.resseqnum2=int(dat[4])
             self.atom2=dat[5]
             self.degrees=float(dat[6])
+        elif self.angle=='LINK':
+            self.segname1=dat[1]
+            self.resseqnum1=int(dat[2])
+            self.atom1=dat[3]
+            self.segname2=dat[4]
+            self.resseqnum2=int(dat[5])
+            self.atom2=dat[6]
+            self.degrees=float(dat[7])
+        elif self.angle=='ANGLEIJK':
+            self.segnamei=dat[1]
+            self.resseqnumi=int(dat[2])
+            self.atomi=dat[3]
+            self.segnamejk=dat[4]
+            self.resseqnumj=int(dat[5])
+            self.atomj=dat[6]
+            self.resseqnumk=int(dat[7])
+            self.atomk=dat[8]
+            self.degrees=float(dat[9])
         else:
             print('Error: unable to parse Crot argument {}'.format(record))
     def replicate(self,newc='',newsegname=''):
@@ -50,11 +66,25 @@ class Crot:
         elif self.angle=='CHI1' or self.angle=='CHI2':  # this is a side-chain bond
             retstr+='set r1 [[atomselect {} "chain {} and resid {} and name CA"] get residue]\n'.format(molid,self.chainID,self.resseqnum1)
             retstr+='SCrot_{} $r1 {} {} {}\n'.format(self.angle.lower(),self.chainID,molid,self.degrees)
-        elif self.angle=='GLYCAN':
-            retstr+='set sel [atomselect {} "segname {}"]\n'.format(molid,self.segname)
-            retstr+='set i [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname,self.resseqnum1,self.atom1)
-            retstr+='set j [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname,self.resseqnum2,self.atom2)
+        elif self.angle=='GLYCAN':  # intra-glycan rotation
+            retstr+='set sel [atomselect {} "segname {}"]\n'.format(molid,self.segname1)
+            retstr+='set i [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname1,self.resseqnum1,self.atom1)
+            retstr+='set j [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname2,self.resseqnum2,self.atom2)
             retstr+='genbondrot {} $sel $i $j {}\n'.format(molid,self.degrees)
+        elif self.angle=='LINK': # ASN-GLYcan rotation
+            retstr+='set sel [atomselect {} "segname {} {}"]\n'.format(molid,self.segname1,self.segname2)
+            retstr+='set i [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname1,self.resseqnum1,self.atom1)
+            retstr+='set j [[atomselect {} "segname {} and resid {} and name {}"] get index]\n'.format(molid,self.segname2,self.resseqnum2,self.atom2)
+            retstr+='genbondrot {} $sel $i $j {}\n'.format(molid,self.degrees)
+        elif self.angle=='ANGLEIJK':
+            retstr+='set rotsel [atomselect {} "segname {}"]\n'.format(molid,self.segnamejk)
+            retstr+='set ri [lindex [atomselect {} "segname {} and resid {} and name {}] get {x y z}] 0]\n'.format(molid,self.segnamei,self.reseqnumi,self.atomi)
+            retstr+='set rj [lindex [atomselect {} "segname {} and resid {} and name {}] get {x y z}] 0]\n'.format(molid,self.segnamej,self.reseqnumj,self.atomj)
+            retstr+='set rk [lindex [atomselect {} "segname {} and resid {} and name {}] get {x y z}] 0]\n'.format(molid,self.segnamek,self.reseqnumk,self.atomk)
+            retstr+='set rij [vecsub $ri $rj]\n'
+            retstr+='set rjk [vecsub $rj $rk]\n'
+            retstr+='set cijk [veccross $rij $rjk]\n'
+            retstr+='$rotsel move [trans center $rj origin $rj axis $cijk {} degrees]\n'.format(self.degrees)
         else:
             print('Warning: buggy crot {}'.format(self.record))
         return retstr
