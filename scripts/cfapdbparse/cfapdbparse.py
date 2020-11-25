e600f5401ce836a3409b4b2b9d3db39c712f1ffa
Abrams <cfa22@drexel.edu>
2020-11-25 11:41:23 -0500

    7jji

--- scripts/cfapdbparse/cfapdbparse.py
+++ scripts/cfapdbparse/cfapdbparse.py
@@ -324,7 +324,7 @@ if __name__=='__main__':
                         'atom ANE5 C10 C','atom ANE5 C11 CT','atom ANE5 N5 N','atom ANE5 O1A O11',
                         'atom ANE5 O1B O12','atom ANE5 O10 O','atom VCG C01 C1','atom VCG C01 C1','atom VCG C02 C2',
                         'atom VCG C03 C3','atom VCG C04 C4','atom VCG C05 C5','atom VCG C06 C6','atom VCG C07 C7',
-                        'atom VCG C08 C8','atom VCG C09 C9']
+                        'atom VCG C08 C8','atom VCG C09 C9','residue EIC LIN']
     PostMod={}
     PostMod['center_protein']=True
     prefix='x01_'
