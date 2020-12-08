aad33c997d55a0cf0246c3da2c084f2266bc24ac
Cameron Abrams <cfa22@drexel.edu>
2020-12-07 20:41:00 -0500

    7jji

---
+++ 7jji/glycan-extract.py
@@ -0,0 +1,44 @@
+#!/home/cfa/anaconda3/bin/python
+
+with open('7jji.pdb','r') as fp:
+   data=fp.read()
+   lines=data.split('\n')
+
+
+links=[]
+for l in lines:
+    if l[0:4]=='LINK':
+        if l[13:16]=='ND2':
+            links.append(l)
+
+gtyp={}
+ref={}
+gtyp[17]='c'
+gtyp[61]='h'
+gtyp[122]='h'
+gtyp[149]='c'
+gtyp[165]='c'
+gtyp[234]='o'
+gtyp[282]='c'
+gtyp[331]='c'
+gtyp[343]='c'
+gtyp[616]='c'
+gtyp[709]='o'
+gtyp[717]='h'
+gtyp[801]='h'
+gtyp[1074]='h'
+gtyp[1098]='c'
+gtyp[1134]='c'
+ref['o']='2wah.pdb,C:1-9,1'
+ref['h']='4b7i.pdb,C:1-8,1'
+ref['c']='4byh.pdb,C:1-10,1'
+
+for l in links:
+    n=int(l[22:26])
+    gt=gtyp[n]
+    gc=l[51:52]
+    gr=int(l[52:56])
+    outstr=ref[gt]+',{}:{},{}'.format(gc,gr,(0 if gr == 1 else gr+1100))
+    print(outstr)
+    
+
