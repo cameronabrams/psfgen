699c4d416b32b0afa4977e8464ad997a7ec52546
Cameron Abrams <cfa22@drexel.edu>
2021-05-14 15:59:28 -0400

    rmsf stuff

--- scripts/delta-rmsf-plot.py
+++ scripts/delta-rmsf-plot.py
@@ -22,25 +22,45 @@ p.add_argument('-rmsfA',type=str,default=[],action='append',help='set A rmsfs')
 p.add_argument('-rmsfB',type=str,default=[],action='append',help='set B rmsfs')
 p.add_argument('-rmsfC',type=str,default=[],action='append',help='set C rmsfs')
 p.add_argument('-rmsfD',type=str,default=[],action='append',help='set D rmsfs')
+p.add_argument('-D',type=str,default='A-B',help='delta operation direction')
 p.add_argument('-labelAB',type=str,default='A/B',help='A-B label')
 p.add_argument('-labelCD',type=str,default='C/D',help='C-D label')
 p.add_argument('-delta-label',type=str,default='',help='delta label')
 p.add_argument('-o',type=str,default='plot.png',help='output plot name')
 
 args=p.parse_args()
+postname,prename=['post','pre']
+if '-' in args.delta_label:
+    postname,prename = args.delta_label.split('-')
 
 resA,msfA=load_and_average(args.rmsfA)
 resB,msfB=load_and_average(args.rmsfB)
-dABmsf=msfA-msfB
+if args.D == 'A-B':
+    dABmsf=msfA-msfB
+else:
+    dABmsf=msfB-msfA
 resC,msfC=load_and_average(args.rmsfC)
 resD,msfD=load_and_average(args.rmsfD)
-dCDmsf=msfC-msfD
+if args.D == 'A-B':
+    dCDmsf=msfC-msfD
+else:
+    dCDmsf=msfD-msfC
 
-fig,ax=plt.subplots(1,1,figsize=(6,5))
-ax.set_xlabel('Residue Number')
-ax.set_ylabel('ΔRMSF {:s} (Å)'.format(args.delta_label))
+fig,ax=plt.subplots(3,1,figsize=(6,10),sharex=True)
+ax[2].set_xlabel('Residue Number')
+ax[0].set_ylabel('ΔRMSF {:s} (Å)'.format(args.delta_label))
+ax[1].set_ylabel('RMSF (Å)')
+ax[2].set_ylabel('RMSF (Å)')
 
-ax.plot(resA,dABmsf,label=args.labelAB)
-ax.plot(resA,dCDmsf,label=args.labelCD)
-ax.legend()
+ax[0].plot(resA,dABmsf,label=args.labelAB+' ({:.2f} Å$^2$)'.format(np.square(dABmsf).mean()))
+ax[0].plot(resA,dCDmsf,label=args.labelCD+' ({:.2f} Å$^2$)'.format(np.square(dCDmsf).mean()))
+ax[0].legend()
+ax[1].set_ylim([0,4])
+ax[1].plot(resA,msfA,label=(postname if args.D=='A-B' else prename)+'-'+args.labelAB)
+ax[1].plot(resA,msfB,label=(prename if args.D=='A-B' else postname)+'-'+args.labelAB)
+ax[1].legend()
+ax[2].set_ylim([0,4])
+ax[2].plot(resA,msfC,label=(postname if args.D=='A-B' else prename)+'-'+args.labelCD)
+ax[2].plot(resA,msfD,label=(prename if args.D=='A-B' else postname)+'-'+args.labelCD)
+ax[2].legend()
 plt.savefig(args.o,bbox_inches='tight')
--- scripts/rmsd.tcl
+++ scripts/rmsd.tcl
@@ -7,16 +7,27 @@
 # convention that a string value for these two arguments has dashes
 # in place of spaces, and split and join commands are used to remove them.
 
+proc .. {from to} {
+    if {$from >= $to} {
+        for {set i $from} {$i <= $to} {incr i}    {lappend out $i}
+    } else {
+        for {set i $from} {$i >= $to} {incr i -1} {lappend out $i}
+    }
+    return $out
+}
+
 proc main { argv } {
     set psf ""
     set sel_measure ""
     set sel_align ""
+    set max_frames 0
     set argc [llength $argv]
     puts "$argv"
     flush stdout
     set dcd []
     set rmsd_outfile "rmsd.dat"
     set rmsf_outfile "rmsf.dat"
+    set rmsf_basis "all"
     for { set i 0 } { $i < $argc } { incr i } {
         if { [lindex $argv $i] == "-psf" } {
             incr i
@@ -26,9 +37,17 @@ proc main { argv } {
             incr i
             lappend dcd [lindex $argv $i]
         }
+        if { [lindex $argv $i] == "-rmsf-basis" } {
+            incr i
+            set rmsf_basis [lindex $argv $i]
+        }
         if { [lindex $argv $i] == "-sel-measure" } {
             incr i
             set sel_measure [join [split [lindex $argv $i] -] " "]
+        }
+        if { [lindex $argv $i] == "-max-frames" } {
+            incr i
+            set max_frames [lindex $argv $i]
         }
         if { [lindex $argv $i] == "-chain-align" } {
             incr i
@@ -72,7 +91,11 @@ proc main { argv } {
     set a [atomselect top $sel_align]
     set m [atomselect top $sel_measure]
     set fp [open $rmsd_outfile "w"]
-    for {set i 0} {$i < [molinfo top get numframes]} {incr i} {
+    set frame_lim [molinfo top get numframes]
+    if { $max_frames > 0 && $max_frames < $frame_lim } {
+        set frame_lim $max_frames
+    }
+    for {set i 0} {$i < $frame_lim } {incr i} {
         $a frame $i
         $m frame $i
         $a move [measure fit $a $a0]
@@ -81,12 +104,36 @@ proc main { argv } {
     close $fp
     puts "Created $rmsd_outfile"
 
-    set rmsf_sel_measure "$sel_measure and name CA"
-    puts "$rmsf_sel_measure"
+    if { $rmsf_basis == "CA" } {
+        set rmsf_sel_measure "$sel_measure and name CA"
+    } else {
+        set rmsf_sel_measure "$sel_measure"
+    }
     set rmsf_m [atomselect top $rmsf_sel_measure]
-    set rmsf [measure rmsf $rmsf_m]
+    set rmsf_per_atom [measure rmsf $rmsf_m first 0 last [expr $frame_lim - 1]]
+    set resid_per_atom [$rmsf_m get resid]
+    set mass_per_atom [$rmsf_m get mass]
+    set resid [lsort -unique $resid_per_atom]
+    set nresid [llength $resid]
+    set mmsf_per_residue [list]
+    set mass_per_residue [list]
+    set rmsf_per_residue [list] 
+    for {set i 0} {$i < $nresid} {incr i} {
+        lappend mmsf_per_residue 0.0
+        lappend mass_per_residue 0.0
+        lappend rmsf_per_residue 0.0
+    }
+    foreach d $rmsf_per_atom r $resid_per_atom m $mass_per_atom {
+        set this_msf_by_atom [expr $d*$d]
+        set i [lsearch $resid $r]
+        lset mmsf_per_residue $i [expr [lindex $mmsf_per_residue $i] + $m*$this_msf_by_atom]
+        lset mass_per_residue $i [expr [lindex $mass_per_residue $i] + $m]
+    }
+    for {set i 0} {$i < $nresid} {incr i} {
+        lset rmsf_per_residue $i [expr sqrt([lindex $mmsf_per_residue $i]/[lindex $mass_per_residue $i])]
+    }
     set fp [open $rmsf_outfile "w"]
-    foreach r [$rmsf_m get resid] f $rmsf {
+    foreach r $resid f $rmsf_per_residue {
         puts $fp "$r [format %.4f $f]"
     }
     close $fp
