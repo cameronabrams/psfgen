3d54516938fd657509065be901e9462a8522c80e
Cameron Abrams <cfa22@drexel.edu>
2021-08-10 13:35:51 -0400

    trr

--- scripts/my_trr2dcd.sh
+++ scripts/my_trr2dcd.sh
@@ -77,7 +77,7 @@ dt=`grep ^dt tmp.mdp|awk '{print $3}'`
 nstxout=`grep ^nstxout tmp.mdp|awk '{print $3}'`
 nsteps=`grep ^nsteps tmp.mdp|awk '{print $3}'`
 
-nframes_expected=`echo $nsteps / $nstxout + 1|bc`
+nframes_expected=`echo "($nsteps/$nstxout)+1"|bc`
 b=0
 
 if [ -f $DCD ]; then
