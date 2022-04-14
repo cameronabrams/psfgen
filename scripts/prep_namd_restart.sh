9e221a5da3bbdedfa877649ffc17a052e6cf06c4
Cameron Abrams <cfa22@drexel.edu>
2022-04-14 12:27:36 -0400

    x

--- scripts/prep_namd_restart.sh
+++ scripts/prep_namd_restart.sh
@@ -99,6 +99,7 @@ fi
 #echo "CURRENT_ENSEMBLE: $CURRENT_ENSEMBLE"
 #exit
 stepsrun=`grep "EXTENDED SYSTEM TO RESTART" $LOG | tail -1 | awk '{print $NF}'`
+echo "stepsrun $stepsrun"
 if [ -z "${stepsrun}" ]; then
     echo "Error: Cannot determine checkpoint timestep from $LOG"
     exit 1
@@ -126,7 +127,9 @@ else
         stepsrequested=$(($stepsrequested+$firsttimestep))
     fi
     stepsleft=$(($stepsrequested-$stepsrun))
+    echo "stepsleft $stepsleft"
     stepsleft=$(($stepsleft+$USERADDSTEPS))
+    echo "stepsleft $stepsleft"
 fi
 lastout=`grep "set outputname" $CONF | awk '{print $3}' | sed 's/;$//'`
 if [ -z "${lastout}" ]; then
